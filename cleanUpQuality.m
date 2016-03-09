3a83571af110096b4954b55ae1cc46d131c5a757
Benjamin Kanter <benjamin.kanter@ntnu.no>
2016-03-09 13:33:16 +0100

    add cleanUpQuality.m

---
+++ cleanUpQuality.m
@@ -0,0 +1,38 @@
+
+
+function raw = cleanUpQuality(raw,labels,groupNames,numGroups,sessions,numSesh,badQ,offQ)
+
+%% switch erroneous offQ to badQ
+temp = {};
+for iSession = 1:numSesh
+    currentData = getColComp(raw,labels,':','session',sessions{iSession});
+    qToChange = getColComp(currentData,labels,'cell num','quality',offQ,'mean rate','>=',0.1);
+    if ~iscell(qToChange)
+        qToChange = {qToChange};
+    end
+    for iCluster = 1:length(qToChange)
+        currentData(strcmpi(currentData(:,strcmpi('cell num',labels)),qToChange{iCluster}), ...
+            strcmpi('quality',labels)) = {badQ};
+    end
+    temp = [temp; currentData];
+end
+raw = temp;
+
+%% remove off quality in all sessions
+clusterNums = unique(getCol(raw,labels,'cell num'),'stable');
+numClusters = length(clusterNums);
+for iCluster = 1:numClusters
+    quality = getColComp(raw,labels,'quality','cell num',clusterNums{iCluster});
+    if sum(strcmpi(quality,offQ)) == length(quality)
+        raw = deleteRowsComp(raw,labels,'cell num',clusterNums{iCluster});
+    end
+end
+
+%% remove bad quality in first session
+toRemove = getColComp(raw,labels,'cell num','session',sessions{1},'quality',badQ);
+if ~iscell(toRemove)
+    toRemove = {toRemove};
+end
+for iCluster = 1:length(toRemove)
+    raw = deleteRowsComp(raw,labels,'cell num',toRemove{iCluster});
+end
